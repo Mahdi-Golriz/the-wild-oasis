1a50011a5dc6a3da3b1b70ca4a344e3e51739ddc
Mahdi-Golriz <mahdi.golriz.1993@gmail.com>
2024-06-12 23:12:59 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Account from "./pages/Account";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import Settings from "./pages/Settings";
+import Users from "./pages/Users";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate replace to="dashboard" />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="account" element={<Account />} />
+        <Route path="bookings" element={<Bookings />} />
+        <Route path="cabins" element={<Cabins />} />
+        <Route path="login" element={<Login />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="users" element={<Users />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/ui/Row.jsx
@@ -0,0 +1,24 @@
+import styled, { css } from "styled-components";
+
+const Row = styled.div`
+  display: flex;
+
+  ${(props) =>
+    props.type === "horizontal" &&
+    css`
+      align-items: center;
+      justify-content: space-between;
+    `}
+  ${(props) =>
+    props.type === "vertical" &&
+    css`
+      flex-direction: column;
+      gap: 1.6rem;
+    `}
+`;
+
+Row.defaultProps = {
+  type: "vertical",
+};
+
+export default Row;
