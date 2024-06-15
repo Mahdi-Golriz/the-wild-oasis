da6864d3c4572cb6c817b057ac346953eb9611eb
Mahdi-Golriz <mahdi.golriz.1993@gmail.com>
2024-06-15 18:46:53 +0200

    ReactQSetup-1

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,21 @@ import Settings from "./pages/Settings";
 import Users from "./pages/Users";
 import AppLayout from "./ui/AppLayout";
 import GlobalStyles from "./styles/GlobalStyles";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -29,7 +40,7 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://gemymjaatpnwnmacsgrn.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdlbXltamFhdHBud25tYWNzZ3JuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg0NTg0ODIsImV4cCI6MjAzNDAzNDQ4Mn0.yC3-qw6bAKp9HA9BqWZ54SwNW1vctWSxCZptu2MO47k";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
